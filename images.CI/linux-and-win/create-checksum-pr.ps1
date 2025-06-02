8438ed1e9cdae221488f75e45e15bfd184f27554
Subir Ghosh <subir0071@github.com>
2025-06-02 15:27:00 -0600

    feat: Add create checksum script

---
+++ images.CI/linux-and-win/create-checksum-pr.ps1
@@ -0,0 +1,12 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$PackerLogPath
+)
+
+
+$logContent = Get-Content -Path $PackerLogPath -Raw
+$SearchString = "Checksum verification failed:"
+
+$matchingLines = Select-String -Path $PackerLogPath -Pattern ([Regex]::Escape($SearchString))
+
+echo $matchingLines
